f6efa4fce8a1128c8ece371c5a72e70286e83017
Melvin Carvalho <melvincarvalho@gmail.com>
2015-07-21 00:52:29 +0200

    two queries

--- create.php
+++ create.php
@@ -20,6 +20,30 @@ try {
   created_at TIMESTAMP NOT NULL
   )";
 
+
+  // use exec() because no results are returned
+  $conn->exec($sql);
+  echo "Tables created successfully";
+}
+catch(PDOException $e)
+{
+  echo $sql . "<br>" . $e->getMessage();
+}
+
+$conn = null;
+
+
+
+
+
+
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
   // sql to create table
   $sql = "CREATE TABLE webid (
   id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
@@ -42,4 +66,14 @@ catch(PDOException $e)
 }
 
 $conn = null;
+?>
+
+
+
+
+
+
+
+
+
 ?>
