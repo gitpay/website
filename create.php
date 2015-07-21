c74dfae54f44f75ee1ec9a31e70dfa46cc26abcb
Melvin Carvalho <melvincarvalho@gmail.com>
2015-07-22 00:49:25 +0200

    followers table

--- create.php
+++ create.php
@@ -1,16 +1,16 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "gitpay";
+
+include_once('dbconfig.php');
+$dsn = "mysql:host=$host;dbname=$db";
+$dbh = new PDO($dsn, $username, $password);
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-  // set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-  // sql to create table
-  $sql = "CREATE TABLE users (
+  // set the PDO error mode to exception
+  $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  $sql = <<<EOSQL
+  CREATE TABLE users (
   id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   login VARCHAR(255) NOT NULL,
   name VARCHAR(255) NULL,
@@ -20,62 +20,91 @@ try {
   avatar_url VARCHAR(255) NULL,
   blog VARCHAR(255) NULL,
   created_at TIMESTAMP NOT NULL
-  )";
+  ) ENGINE=InnoDB
+EOSQL;
 
+  $r = $dbh->exec($sql);
 
-  // use exec() because no results are returned
-  $conn->exec($sql);
-  echo "Tables created successfully";
+  $msg = '';
+  if ($r !== false) {
+    $msg =  "Tables are created successfully!<br/>";
+  } else{
+    $msg =  "Error creating the users table.<br/>";
+  }
+
+  // display the message
+  if($msg !== '') {
+    echo $msg;
+  }
+
+} catch (PDOException $e){
+  echo $e->getMessage();
 }
-catch(PDOException $e)
-{
-  echo $sql . "<br>" . $e->getMessage();
-}
-
-$conn = null;
-
-
-
-
 
 
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+
   // set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-
-  // sql to create table
-  $sql = "CREATE TABLE webid (
+  $sql = <<<EOSQL
+  CREATE TABLE webid (
   id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   login VARCHAR(255) NOT NULL,
   bitcoin VARCHAR(255) NULL,
   bitmark VARCHAR(255) NULL,
   preferredURI VARCHAR(255) NULL,
   created_at TIMESTAMP NOT NULL
-  )";
+  ) ENGINE=InnoDB
+EOSQL;
 
 
+  $r = $dbh->exec($sql);
 
-  // use exec() because no results are returned
-  $conn->exec($sql);
-  echo "Tables created successfully";
+  if ($r !== false){
+    $msg =  "Tables are created successfully!<br/>";
+  } else {
+    $msg =  "Error creating the webid table.<br/>";
+  }
+
+  // display the message
+  if($msg !== '') {
+    echo $msg;
+  }
+
+} catch (PDOException $e){
+  echo $e->getMessage();
 }
-catch(PDOException $e)
-{
-  echo $sql . "<br>" . $e->getMessage();
+
+
+try {
+
+  // set the PDO error mode to exception
+  $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  $sql = <<<EOSQL
+  CREATE TABLE followers (
+  follower_id INT(11),
+  user_id INT(11),
+  created_at TIMESTAMP NOT NULL
+  ) ENGINE=InnoDB
+EOSQL;
+
+
+  $r = $dbh->exec($sql);
+
+  if ($r !== false){
+    $msg =  "Tables are created successfully!<br/>";
+  } else {
+    $msg =  "Error creating the webid table.<br/>";
+  }
+
+  // display the message
+  if($msg !== '') {
+    echo $msg;
+  }
+
+} catch (PDOException $e){
+  echo $e->getMessage();
 }
-
-$conn = null;
-?>
-
-
-
-
-
-
-
-
-
-?>
---
+++ dbconfig.php
@@ -0,0 +1,5 @@
+<?php
+$host='localhost';
+$db = 'gitpay';
+$username = 'root';
+$password = '';
