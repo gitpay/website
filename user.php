9010d5359bc4f8450047597bd3a6395291c4d91b
Melvin Carvalho <melvincarvalho@gmail.com>
2015-07-31 09:31:15 +0200

    add avatar

--- user.php
+++ user.php
@@ -156,6 +156,7 @@ $githubaccount = 'http://github.com/' . $user['login'];
 
 $turtle = "<#this> a <http://xmlns.com/foaf/0.1/Person> ;\n";
 $turtle .= "<http://xmlns.com/foaf/0.1/name> '$user[name]' ;\n";
+$turtle .= "<http://xmlns.com/foaf/0.1/img> '$user[avatar_url]' ;\n";
 $turtle .= "<http://xmlns.com/foaf/0.1/account> <https://github.com/$user[login]> .\n";
 
 if ($preferredURI) {
