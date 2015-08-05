01d3fc28991e3c94fb0f1b1bf643b1c144d48281
Melvin Carvalho <melvincarvalho@gmail.com>
2015-08-05 03:09:46 +0200

    github config

---
+++ github.php
@@ -0,0 +1,13 @@
+<?php
+
+if (!isset($cache)) {
+  $cache = './data/github-api-cache';
+}
+
+$client = new \Github\Client(
+    new \Github\HttpClient\CachedHttpClient(array('cache_dir' => $cache))
+);
+
+@include('api.php');
+
+?>
