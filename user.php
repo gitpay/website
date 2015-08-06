ae5db28ef855b2c427708e1edc848f13cf54a89a
Melvin Carvalho <melvincarvalho@gmail.com>
2015-08-06 23:31:15 +0200

    followers

--- user.php
+++ user.php
@@ -155,6 +155,13 @@ if (!$user) {
 
 try {
   $users = $client->api('user')->followers($nick);
+  for ($i=0; $i<sizeof($users); $i++) {
+    $fid = $users[$i]['id']
+    $sql = "insert into users values ($user[id], $fid, NULL, DEFAULT) ; ";
+    error_log($sql);
+    //$stmt = $conn->prepare($sql);
+    //$stmt->execute();
+  }
 
 }
 catch(Exception $e)
