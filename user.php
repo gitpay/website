c97a327acef6bb921308ae340770e1cfa01f908c
Melvin Carvalho <melvincarvalho@gmail.com>
2015-08-06 15:41:29 +0200

    add error logs for 503s

--- user.php
+++ user.php
@@ -67,7 +67,7 @@ if (!$user) {
   }
   catch(Exception $e)
   {
-    //error_log('api error for user : ' + $nick);
+    error_log('api error for user : ' + $nick);
     header('HTTP/1.1 503 Service Temporarily Unavailable');
     header('Status: 503 Service Temporarily Unavailable');
     header('Retry-After: 3600');//300 seconds
@@ -101,6 +101,7 @@ try {
 }
 catch(Exception $e)
 {
+  error_log('api error for followers of : ' + $nick);
   header('HTTP/1.1 503 Service Temporarily Unavailable');
   header('Status: 503 Service Temporarily Unavailable');
   header('Retry-After: 3600');//300 seconds
@@ -116,6 +117,7 @@ try {
 }
 catch(Exception $e)
 {
+  error_log('api error for keys of user : ' + $nick);
   header('HTTP/1.1 503 Service Temporarily Unavailable');
   header('Status: 503 Service Temporarily Unavailable');
   header('Retry-After: 3600');//300 seconds
