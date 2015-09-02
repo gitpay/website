85fe597e88d080a97fd62162a041f24684032826
Melvin Carvalho <melvincarvalho@gmail.com>
2015-09-02 03:35:18 +0200

    link to spkac generator

--- keygen.php
+++ keygen.php
@@ -17,8 +17,10 @@ Your signed public key and challenge (spkac) is:
 
 <?php echo $spkac; ?>
 
+
 Please use this value to locally generate ssh / x.509 certificates and upload to github and your browser.
 
+<a href="http://phpmylogin.sourceforge.net/wiki/doku.php?id=keygen_attribute">seeAlso</a>
 
 </body>
 </html>
