110ac932483d5384712f8c27c5b4174cbc8fdf60
Melvin Carvalho <melvincarvalho@gmail.com>
2015-09-02 03:44:27 +0200

    spkac examples

--- keygen.php
+++ keygen.php
@@ -22,5 +22,39 @@ Please use this value to locally generate ssh / x.509 certificates and upload to
 
 <a href="http://phpmylogin.sourceforge.net/wiki/doku.php?id=keygen_attribute">seeAlso</a>
 
+
+Example SPKAC
+=============
+
+SPKAC=<?php echo $spkac; ?>
+CN=Your Name
+emailAddress=your.name@mail.com
+0.OU=Company client certificate
+organizationName=Company
+countryName=NO
+stateOrProvinceName=Oslo
+localityName=Oslo
+
+Save to /tmp/tmp.spkac
+
+
+Example openssl sig
+====================
+
+openssl ca -config /etc/CA/openssl.conf -days 36500 -notext -batch -spkac /tmp/tmp.spkac -out /tmp/tmp
+
+
+Example install script
+======================
+
+$file = '/tmp/tmp';
+$length = filesize($file);
+header('Last-Modified: '.date('r+b'));
+header('Accept-Ranges: bytes');
+header('Content-Length: '.$length);
+header('Content-Type: application/x-x509-user-cert');
+readfile($file);
+exit;
+
 </body>
 </html>
