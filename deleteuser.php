0bfcd2b27fbe68d41504ca055b161c226214e50e
Yan Shcherbakov <yanshcherbakov@Yans-MacBook-Pro.local>
2016-12-26 10:48:33 -0500

    Adding the files to remove the users.

---
+++ deleteuser.php
@@ -0,0 +1,21 @@
+<?php
+
+// includes
+require_once 'vendor/autoload.php';
+@include_once('server.php');
+include_once('dbconfig.php');
+include_once('github.php');
+include_once('functions.php');
+
+// DB
+$fallbackdb = 'ghtorrent';
+$conn = new PDO("mysql:host=$host;dbname=$db", $username, $password);
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$connfb = new PDO("mysql:host=$host;dbname=$fallbackdb", $username, $password);
+$connfb->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === 'true') {
+	deleteUser($_SESSION['login'], $conn);
+}
+
+?>
